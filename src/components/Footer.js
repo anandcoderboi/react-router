22486ae23e3c29c58a8ab7aa4c416170467bd118
amardeepio <amars123456@gmail.com>
2022-11-13 17:09:57 +0530

    added

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyAnand</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+         
+          <a href="https://https://twitter.com/Anandbolly" target={"blank"}>
+            twitter
+          </a>
+          <a href="https://github.com/anandcoderboi/" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default  Footer
